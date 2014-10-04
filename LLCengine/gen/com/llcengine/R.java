9a46e38507c4514362f9456b8c2cf49b54a8e2f5
michael <te0919193534@gmail.com>
2014-10-04 14:25:01 +0800

    reset

--- LLCengine/gen/com/llcengine/R.java
+++ LLCengine/gen/com/llcengine/R.java
@@ -23,7 +23,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int back=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int btn_circle=0x7f020001;
+        public static final int btn_square=0x7f020002;
+        public static final int btn_triangle=0x7f020003;
+        public static final int btn_x=0x7f020004;
+        public static final int grey_circle=0x7f020005;
+        public static final int grey_square=0x7f020006;
+        public static final int grey_tirangle=0x7f020007;
+        public static final int grey_x=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
